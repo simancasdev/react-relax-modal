7bbe1058ef195366414edf093acdc869e0248605
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2023-12-12 19:12:38 -0400

    [bugfix][type] fixing closeKey type

--- src/components/Modal/interfaces/config.ts
+++ src/components/Modal/interfaces/config.ts
@@ -3,13 +3,13 @@ import {Alignment, CloseButton} from ".";
 import {Target, TargetAndTransition} from "framer-motion";
 
 export type RelaxModalConfig = {
-  closeKey: "Escape";
   alignment: Alignment;
   backgroundColor: string;
   closeOnBackdrop: boolean;
   autoCloseMS: number | undefined;
   style: CSSProperties | undefined;
   closeButton: Partial<CloseButton>;
+  closeKey: "Escape" | (string & {});
   onClosed: (() => void) | undefined;
   onBackdrop: (() => void) | undefined;
   animation: {
