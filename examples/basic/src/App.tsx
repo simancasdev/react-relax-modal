d96331d1a06cc3dda162a4b9e0a9ff8f4479405e
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-03-17 17:05:10 -0400

    adding basic usage

--- examples/basic/src/App.tsx
+++ examples/basic/src/App.tsx
@@ -1,8 +1,34 @@
+import {CSSProperties} from "react";
+import {RelaxModalProvider, useRelaxModal} from "react-relax-modal";
+
+const modalStyle: CSSProperties = {
+  width: 200,
+  height: 200,
+  display: "flex",
+  borderRadius: "5px",
+  alignItems: "center",
+  justifyContent: "center",
+  backgroundColor: "white",
+};
+
+const MyModal = () => {
+  return (
+    <div style={modalStyle}>
+      <span>Hello World!</span>
+    </div>
+  );
+};
+
+const Screen = () => {
+  const {openModal} = useRelaxModal();
+  return <button onClick={() => openModal(<MyModal />)}>Open My Modal</button>;
+};
+
 function App() {
   return (
-    <div>
-      <button>Open modal!</button>
-    </div>
+    <RelaxModalProvider>
+      <Screen />
+    </RelaxModalProvider>
   );
 }
 
