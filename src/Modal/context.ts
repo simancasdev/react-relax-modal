392125e2e8ccf19450ad4d1d3c47bbce4d48cd11
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-03-17 17:19:56 -0400

    formatting error message

--- src/Modal/context.ts
+++ src/Modal/context.ts
@@ -9,9 +9,9 @@ export const useRelaxModal = (): IContext => {
   const context = useContext(RelaxModalContext);
   if (typeof context === "undefined")
     throw Error(`
-  You have to wrap the components which you want 
-  to get access with a <RelaxModalProvider>...</RelaxModalProvider> 
-`);
+      You have to wrap the components which you want 
+      to get access with a <RelaxModalProvider>...</RelaxModalProvider> 
+    `);
 
   return context;
 };
