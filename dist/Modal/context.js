30b66a4cf8056f8568e4ad0cf62b3542d9a81c7c
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-03-17 17:25:53 -0400

    building patch 🤦🏿

--- dist/Modal/context.js
+++ dist/Modal/context.js
@@ -4,8 +4,8 @@ export const useRelaxModal = () => {
     const context = useContext(RelaxModalContext);
     if (typeof context === "undefined")
         throw Error(`
-  You have to wrap the components which you want 
-  to get access with a <RelaxModalProvider>...</RelaxModalProvider> 
-`);
+      You have to wrap the components which you want 
+      to get access with a <RelaxModalProvider>...</RelaxModalProvider> 
+    `);
     return context;
 };
