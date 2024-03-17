30d25ec90fb1ad134f722b1678284042150947ab
“simancasdev” <129432908+simancasdev@users.noreply.github.com>
2024-03-16 23:12:01 -0400

    removing specs folder from dist

--- dist/specs/mock-components/Basic.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react" />
-interface BasicProps {
-}
-export declare const Basic: React.FC<BasicProps>;
-export {};
--- dist/specs/mock-components/Basic.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { useRelaxModal } from "index";
-export const Basic = () => {
-    const { openModal } = useRelaxModal();
-    return (_jsx("div", { children: _jsx("button", { onClick: () => openModal(_jsx("div", {})) }) }));
-};
